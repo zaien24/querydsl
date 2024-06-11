e55b1f285378efa46a95de55f06f84392b8e58dd
yuyuhi <zaien24@naver.com>
2024-06-11 12:21:26 +0900

    섹션3 Case 문

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -384,15 +384,49 @@ public class QuerydslBasicTest {
         QMember memberSub = new QMember("memberSub");
 
         List<Tuple> fetch = queryFactory
-            .select(member.username,
-                    JPAExpressions
-                            .select(memberSub.age.avg())
-                            .from(memberSub)
-            ).from(member)
-            .fetch();
+                .select(member.username,
+                        JPAExpressions
+                                .select(memberSub.age.avg())
+                                .from(memberSub)
+                ).from(member)
+                .fetch();
 
         for (Tuple tuple : fetch) {
             System.out.println("tuple = " + tuple);
 
         }
+    }
+
+    @Test
+    public void basicCase() {
+        List<String> result = queryFactory
+                .select(member.age
+                        .when(10).then("열살")
+                        .when(20).then("스무살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void complexCase() {
+        List<String> result = queryFactory
+                .select(new CaseBuilder()
+                        .when(member.age.between(0, 20)).then("0~20살")
+                        .when(member.age.between(21, 30)).then("21~30살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+        
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+
+    }
+
+
 }
